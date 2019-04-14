4d4e81cd9c5d7cfe354ed0b1cf06bb71135e2f15
Dirk Adler <dirk.adler@idealo.de>
2019-04-14 20:49:39 +0200

    fix travis config

--- travis.sh
+++ travis.sh
@@ -8,7 +8,7 @@ git clone --depth 1 --branch "$LIBRDKAFKA_VERSION" https://github.com/edenhill/l
     cd librdkafka
     ./configure
     make
-    make install
+    sudo make install
 )
 sudo ldconfig
 
@@ -19,7 +19,7 @@ git clone --depth 1 --branch "PHP-7.4" https://github.com/php/php-src.git
     phpize
     ./configure --with-ffi
     make
-    make install
+    sudo make install
 )
 
 # install pcov
@@ -29,5 +29,5 @@ git clone --depth 1 https://github.com/krakjoe/pcov.git
     phpize
     ./configure --enable-pcov
     make
-    make install
+    sudo make install
 )
