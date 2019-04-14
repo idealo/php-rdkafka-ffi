5c564abe108caca9656f4da6aa6788b84a53449b
Dirk Adler <dirk.adler@idealo.de>
2019-04-14 20:17:06 +0200

    fix travis config
    * test adding ffi & pcov

--- travis.sh
+++ travis.sh
@@ -11,3 +11,23 @@ git clone --depth 1 --branch "$LIBRDKAFKA_VERSION" https://github.com/edenhill/l
     sudo make install
 )
 sudo ldconfig
+
+# install ffi
+git clone --depth 1 --branch "PHP-7.4" https://github.com/php/php-src.git
+(
+    cd php-src/ext/ffi
+    phpize
+    ./configure --with-ffi
+    make
+    sudo make install
+)
+
+# install pcov
+git clone --depth 1 https://github.com/krakjoe/pcov.git
+(
+    cd pcov
+    phpize
+    ./configure --enable-pcov
+    make
+    sudo make install
+)
