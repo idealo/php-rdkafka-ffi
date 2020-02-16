41164d13a3c38ca794c37abd1eb5eea96667c0e7
Dirk Adler <dirk.adler@idealo.de>
2020-02-16 14:40:56 +0100

    Test travis windows kafka setup

--- resources/phpunit/config.php
+++ resources/phpunit/config.php
@@ -1,10 +1,6 @@
 <?php
 
-if (PHP_OS_FAMILY === 'Windows') {
-    define('KAFKA_BROKERS', 'localhost:9092');
-} else {
-    define('KAFKA_BROKERS', 'kafka:9092');
-}
+define('KAFKA_BROKERS', getenv('KAFKA_BROKERS') ?: 'kafka:9092');
 const KAFKA_TEST_TOPIC = "test";
 const KAFKA_TEST_TOPIC_PARTITIONS = "test_partitions";
 const KAFKA_TEST_TIMEOUT_MS = 6000;
