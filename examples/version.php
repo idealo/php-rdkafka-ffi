85003d96671673c01a366fc3571bfdd0575cd53a
Dirk Adler <dirk.adler@idealo.de>
2020-04-16 09:28:49 +0200

    Fix version example

--- examples/version.php
+++ examples/version.php
@@ -6,7 +6,6 @@ use RdKafka\FFI\Api;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-//Api::init('1.0.0');
 $version = getenv('LIBRDKAFKA_VERSION') ?: '';
 $version = ltrim($version, 'v');
 $version = $version === 'master' ? Api::VERSION_LATEST : $version;
@@ -14,4 +13,4 @@ Api::init($version);
 
 echo 'Binding Version (string): ' . Api::getVersion() . PHP_EOL;
 echo 'Library Version (int)   : ' . Api::rd_kafka_version() . PHP_EOL;
-echo 'Library Version (string): ' . FFI::string(Api::rd_kafka_version_str()) . PHP_EOL;
+echo 'Library Version (string): ' . Api::rd_kafka_version_str() . PHP_EOL;
