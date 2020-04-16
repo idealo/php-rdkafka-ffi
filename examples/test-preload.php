eee43455cf5307706652e00d30df6ac373205beb
Dirk Adler <dirk.adler@idealo.de>
2020-04-16 09:33:11 +0200

    Fix preload test example

--- examples/test-preload.php
+++ examples/test-preload.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 $ffiKafka = FFI::scope('RdKafka');
 
 echo 'Version (int)   : ' . $ffiKafka->rd_kafka_version() . PHP_EOL;
-echo 'Version (string): ' . FFI::string($ffiKafka->rd_kafka_version_str()) . PHP_EOL;
+echo 'Version (string): ' . $ffiKafka->rd_kafka_version_str() . PHP_EOL;
